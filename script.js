7a5cb208904098b080638d9c6743b952398cc111
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-09 10:28:53 +0000

    Update

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 let currentPlayer = 'X';
 let arr = Array(9).fill(null);
 
+function checkWin(){
+    if(
+        
+        (arr[0] !== null && arr[0] == arr[1] && arr[2]) ||
+        (arr[3] !== null && arr[3] == arr[4] && arr[5]) ||
+        (arr[6] !== null && arr[6] == arr[7] && arr[8]) ||
+        (arr[0] !== null && arr[0] == arr[3] && arr[6]) ||
+        (arr[1] !== null && arr[1] == arr[4] && arr[7]) ||
+        (arr[2] !== null && arr[2] == arr[5] && arr[8]) ||
+        (arr[0] !== null && arr[0] == arr[4] && arr[8]) ||
+        (arr[2] !== null && arr[2] == arr[4] && arr[6]) 
+
+    ){
+        alert(`Winner is ${currentPlayer}`);
+        return;
+    }
+    
+    if(!arr.some((e)=> e == null)){
+        alert(`Game is Draw!!`);
+        return
+    }
+}
+
+
 function handleCub(el) {
     const id = Number(el.id);
+    if(arr[id] !== null) return;
     arr[id] = currentPlayer;
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    console.log(arr);
+    el.innerText = currentPlayer;
+    checkWin();
+    currentPlayer = currentPlayer === "X" ? "O" : "X"
+
 }
 
