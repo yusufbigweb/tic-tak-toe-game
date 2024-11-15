1e543ae907d9bf11a07b2f7a3fd3171cc18a7fbd
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-15 13:37:29 +0000

    Update

--- script.js
+++ script.js
@@ -23,12 +23,6 @@ function checkWin(){
         return
     }
 }
-
-console.log("Yusuf Shaikh")
-console.log("Yusuf sHAIKH");
-
-
-
 function handleCub(el) {
     const id = Number(el.id);
     if(arr[id] !== null) return;
